29fa27b4353054c7e3b8da1ce6d7bb5db886bf93
pedramkousari <persianped@gmail.com>
2023-07-22 12:36:54 +0330

    init

---
+++ cmd/patch/patch.go
@@ -0,0 +1,30 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package patch
+
+import (
+	"github.com/spf13/cobra"
+)
+
+// patchCmd represents the patch command
+var PatchCmd = &cobra.Command{
+	Use:   "patch",
+	Short: "Create Or Update Patch",
+	Long: ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		cmd.Help()
+	},
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// patchCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// patchCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+/*
+Copyright © 2023 pedram kousari <pedrsianped@gmail.com>
+*/
+package main
+
+import "github.com/pedramkousari/abshar-toolbox/cmd"
+
+func main() {
+	cmd.Execute()
+}
